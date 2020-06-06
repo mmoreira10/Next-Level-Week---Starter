98775e5d19c3874c57a876e80a75880004e60c38
mmoreira10 <matheus_moreira_1095@hotmail.com>
2020-06-06 01:15:21 -0300

    Inserção SQLite + ajustes de bugs e front-end

--- public/scripts/create-point.js
+++ public/scripts/create-point.js
@@ -63,6 +63,8 @@ function handleSelectedItem(event) {
 
     const itemId = itemLi.dataset.id
 
+    // console.log('ITEM ID: ', itemId)
+
     // verificar se existem itens selecionados, se sim
     // pegar os itens selecionados
 
@@ -86,6 +88,8 @@ function handleSelectedItem(event) {
         // adicionar à seleção
         selectedItems.push(itemId)
     }
+
+    // console.log('selectedItems: ', selectedItems)
     // atualizar o campo escondido com os itens selecionados
     collectedItems.value = selectedItems
 }
